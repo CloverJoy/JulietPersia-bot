1f757b6abbd1db9e8aaf4a9178c053ad756c708f
CloverJoy <chris.ang.dev@gmail.com>
2021-03-04 17:16:23 -0800

    Updated version with mtg and covid command

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,31 +13,42 @@ const App = () => (
         <AccordionItem>
             <AccordionButton>
                 <Box flex="1" textAlign="left">
-                    Section 1 title
+                    Hi there, I'm Juliet Persia!
       </Box>
                 <AccordionIcon />
             </AccordionButton>
             <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                If you see this, Juliet Persia will online in 30 minutes. If you want to increase my lifespan, just refresh my website!
     </AccordionPanel>
         </AccordionItem>
 
         <AccordionItem>
             <AccordionButton>
                 <Box flex="1" textAlign="left">
-                    Section 2 title
+                    Invite me to your discord server!
       </Box>
                 <AccordionIcon />
             </AccordionButton>
             <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                click this <a href="https://discord.com/oauth2/authorize?client_id=802744454107758624&scope=bot">LINK</a> to summon me to your server! :D
+                Type ~help. for all the command lists!
     </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+            <AccordionButton>
+                <Box flex="1" textAlign="left">
+                    Command lists:
+      </Box>
+                <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel pb={4}>
+                ~help : show command help <br />
+                ~hello : to greet you <br />
+                ~ani : for anime information <br />
+                ~covid TYPECOUNTRY (example ~covid usa): for covid information <br />
+                ~btc: information about bitcoin today <br />
+                ~mtg CARDNAME (example ~mtg avacyn): for MTG card information <br />
+            </AccordionPanel>
         </AccordionItem>
     </Accordion>
 );
